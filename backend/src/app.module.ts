f5f8ced8fdafc6286529639bfc9728911e00911f
dariofo2 <alejandrodariofo2@gmail.com>
2025-06-25 00:13:11 +0200

    DockerCompose + Web3 Backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Web3Service } from './web3/web3.service';
+import { Web3Module } from './web3/web3.module';
 
 @Module({
-  imports: [],
+  imports: [Web3Module],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Web3Service],
 })
 export class AppModule {}
