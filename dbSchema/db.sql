0d9e817f1203f85ec16aa88876388035df64c5ae
dariofo2 <alejandrodariofo2@gmail.com>
2025-06-25 00:22:29 +0200

    db Schema fix

--- dbSchema/db.sql
+++ dbSchema/db.sql
@@ -8,13 +8,13 @@ CREATE TABLE IF NOT EXISTS accounts (
 CREATE TABLE IF NOT EXISTS transactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     accountId INT,
-    from VARCHAR(256) NOT NULL,
-    to VARCHAR(256) NOT NULL,
+    fromAcc VARCHAR(256) NOT NULL,
+    toAcc VARCHAR(256) NOT NULL,
     value INT NOT NULL,
     block VARCHAR(256),
     hash VARCHAR(256),
-    erc20 TINYINT,
-    contract_address,
+    isErc20 TINYINT,
+    contract_address varchar(256),
     date INT,
-    FOREIGN KEY accountId REFERENCES id (accounts)
+    FOREIGN KEY (accountId) REFERENCES accounts (id)
 );
